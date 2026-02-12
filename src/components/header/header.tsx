5290ec2c00a0e42298f37fc8f3d8c2b5e63a311a
IlyaVafin <rthrthrer5gh@gmail.com>
2026-02-12 21:50:02 +0300

    fix: #6 fix header height

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,15 +4,15 @@ import Container from "../container/container"
 
 export default function Header() {
 	return (
-		<header className='fixed top-0 left-0 pt-5 pb-3.75 w-full border-b border-b-[#FFFFFF4D]'>
-			<Container>
-				<div className='flex items-center justify-between'>
-					<Image src='/logo.svg' alt='logotype' width={70} height={84} />
-					<Button height={500} width={200} color='white'>
-						Связаться с нами
-					</Button>
-				</div>
-			</Container>
+		<header className='relative h-29.75 pt-5 pb-3.75 w-full border-b border-b-[#FFFFFF4D]'>
+			<div className='fixed top-5 left-0 w-full'>
+				<Container>
+					<div className='flex items-center justify-between'>
+						<Image src='/logo.svg' alt='logotype' width={70} height={84} />
+						<Button height={500} width={200} color='white'>Связаться с нами</Button>
+					</div>
+				</Container>
+			</div>
 		</header>
 	)
 }
